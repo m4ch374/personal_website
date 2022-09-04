e1edaa718a03ba7be5dac164079dd76236b3aa38
m4ch373 <spiner47@gmail.com>
2022-09-04 12:42:17 +1000

    Setup: removed footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,20 +14,6 @@ const Home: NextPage = () => {
       <main>
         
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className='text-white'
-        >
-          Powered by{' '}
-          <span className='invert'>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
