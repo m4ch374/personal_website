05d60b32957c90f286f41b90bc941d7d73cfc59e
m4ch373 <spiner47@gmail.com>
2022-09-07 14:46:59 +1000

    Development: Placeholder card

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectCard: React.FC = () => {
+  return (
+    <div className="grid grid-flow-col place-items-center">
+      <div>
+        something 
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
