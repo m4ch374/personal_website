d941bd038dd78eb67381e1904b03e279ccbf6e98
m4ch373 <spiner47@gmail.com>
2022-09-09 12:55:51 +1000

    Deployment: removed slash after repo name

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/personal_website/' : '',
-  assetPrefix: isProd ? '/personal_website/' : ''
+  basePath: isProd ? '/personal_website' : '',
+  assetPrefix: isProd ? '/personal_website' : ''
 }
 
 module.exports = nextConfig
