3790a98c84b7715287ec626cb961879d0f3bcbc1
m4ch373 <spiner47@gmail.com>
2022-09-09 13:13:21 +1000

    Deployment: trailing slash for assetPrefix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: isProd ? '/personal_website' : '',
-  assetPrefix: isProd ? '/personal_website' : ''
+  assetPrefix: isProd ? '/personal_website/' : ''
 }
 
 module.exports = nextConfig
