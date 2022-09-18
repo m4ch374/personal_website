1a657b92f177cc6d3442663c00a8dd33370a26ad
m4ch373 <spiner47@gmail.com>
2022-09-18 19:15:24 +1000

    Development: Added page transitions

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,48 @@
 import React from "react";
 import MenuBar from "./MenuBar";
+import { motion, AnimatePresence } from "framer-motion"
+import { useRouter } from "next/router"
 
 interface ILayout {
   children?: string | JSX.Element | JSX.Element[]
 }
 
 const Layout: React.FC<ILayout> = ({children}) => {
+  const router = useRouter()
+
+  const pageTransition = {
+    out: {
+      opacity: 0,
+      y: 40,
+      transition: {
+        duration: 0.75
+      }
+    },
+    in: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.75,
+        delay: 0.5
+      }
+    }
+  }
+
   return (
-    <div>
+    <div className="overflow-hidden">
       <MenuBar />
-      
-      <main>
-        {children}
-      </main>
+      <AnimatePresence initial={false} mode="wait">
+
+        <motion.div 
+          key={router.asPath}
+          variants={pageTransition}
+          initial="out"
+          animate="in"
+          exit="out"
+        >
+            {children}
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
