9bb1079849198bd1b2274e5167d8d45566369550
m4ch374 <m4ch374@users.noreply.github.com>
2023-01-26 18:28:21 +0000

    deploy: 6cfee9386017f1ddae848da11bf5f0c196884eee

---
+++ _next/static/FCOgms8l1OVeHnBHFUhUo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1c46db94a61a441b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/projects":["static/chunks/pages/projects-4b126e6a689fd48a.js"],sortedPages:["/","/_app","/_error","/projects"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/J3iKjAsN2yvT8JDmKlDix/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-38e98918a615d300.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/projects":["static/chunks/pages/projects-bf807eba5e296ea7.js"],sortedPages:["/","/_app","/_error","/projects"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
