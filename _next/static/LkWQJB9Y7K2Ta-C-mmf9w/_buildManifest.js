739eb5caeab8628d87ca8926eaa97a8646a3630c
m4ch374 <m4ch374@users.noreply.github.com>
2023-06-25 06:15:25 +0000

    deploy: a423e71db92aef5b2b9f3d40d30233849b2334d0

--- _next/static/LkWQJB9Y7K2Ta-C-mmf9w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1abe471f72dca2e3.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/projects":["static/chunks/pages/projects-104309e1087d81aa.js"],sortedPages:["/","/_app","/_error","/projects"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
