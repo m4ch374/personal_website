3ad1905300d71e7bb67e5e6452d27129574e2122
m4ch373 <spiner47@gmail.com>
2023-12-01 23:57:33 +0800

    update(intro): updated intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Paragraph from '../components/Paragraph'
 import LinkButton from '../components/LinkButton'
 import InlineSection from '../components/InlineSection'
 import getPrefix from '../helpers/getPrefix'
+import Experience from '../components/Experience'
 
 const Home: NextPage = () => {
   const prefix = getPrefix()
@@ -27,9 +28,14 @@ const Home: NextPage = () => {
 
           <br /><br />
 
-          I had worked as a programming tutor teaching basic C programming to high school students, it was 
-          a fun experience! I&apos;ve also completed numerous personal projects in my free time and work
-          hard in the university projects.
+          I had tapped into different areas and stumbled upon frontend development which is what I&apos;m currently focusing.
+          Utilizing ReactJS and work on interesting projects to bring excellent UI/UX experience.
+
+          <br /><br />
+
+          I have worked at stomble, where I wrote a custom in-house fetching solution which utilizes the fluent interface design pattern.
+          And also developed a feature-rich reels-like short form video system using React Native. Currently, I am appointed as a frontend
+          director leading the development of an internal hiring and offers system for the CSESoc.
 
           <br /><br />
 
@@ -42,9 +48,48 @@ const Home: NextPage = () => {
         </div>
 
         <Paragraph title="Skills & Experience">
-          <div>
-            Had experiences in both front-end and back-end development. Worked as a programming tutor before.
-          </div>
+          <Experience company='Stomble' jobTitle='Full-Stack Developer' start='Jul 2023' end='present' location='Sydney, AU'>
+            <ul className='list-disc pl-4'>
+              <li>Developed a large-scale, responsive, and user-friendly mobile application using TypeScript, React Native, Expo, Serverless and Prisma.</li>
+              <li>Contributed to standardizing fetching behavior by developing a sophisticated in-house fetcher class by adopting the fluent interface design pattern.</li>
+              <li>Standardized the React Redux storing layout and provided an easily accessible and maintainable dispatcher for other team members to use in their work.</li>
+              <li>Developed a custom eslint rule to further standardize the quality of code across the whole project.</li>
+              <li>Crafted, developed, and maintained an intuitive and feature packed reels-like short form video system, using TailwindCSS and Amazon S3 for reliable and optimized multimedia content delivery.</li>
+              <li>Participated in Agile and SCRUM planning process, assisting the product owner in documenting roadmap and planning future sprints, using JIRA and Confluence.</li>
+            </ul>
+          </Experience>
+
+          <Experience company='UNSW CSESoc' jobTitle='Frontend Director' start='Jul 2023' end='Nov 2023' location='Sydney, AU'>
+            <ul className='list-disc pl-4'>
+              <li>Spearheaded the frontend development of an internal hiring and offers system for the CSESoc. Leading, managing, and coordinating team members to ensure adherence to established timelines</li>
+              <li>Developed and maintained a highly responsive and interactive UI for the hiring system, utilizing TypeScript, React JS, TailwindCSS, MUI and Vite.</li>
+              <li>Overhauled the design for an application subsystem, improving the user experience and optimizing performance for lower quality hardware, leveraging the TDD methodology to ensure code correctness.</li>
+              <li>Participated in the construction of the CI/CD pipelines to ensure the team pushing well-tested and bug free code.</li>
+              <li>Carry out managerial responsibilities utilizing Confluence and JIRA.</li>
+            </ul>
+          </Experience>
+
+          <Experience company='SAS Analytics' jobTitle='Project Team Member' start='May 2023' end='Jul 2023' location='Sydney, AU'>
+            <ul className='list-disc pl-4'>
+              <li>Developed a full stack webapp for financial risk managers to utilize a home loan origination model for risk assessment.</li>
+              <li>Leveraging SAS software suites, developed models which aimed at accurately identifying customers with a high propensity for default</li>
+            </ul>
+          </Experience>
+
+          <Experience company='UNSW CSESoc' jobTitle='Training Program Lead' start='Feb 2023' end='Jul 2023' location='Sydney, AU'>
+            <ul className='list-disc pl-4'>
+              <li>Taking the when2meet application as inspiration, led a team of 5 in developing a website with similar features, while having unparalleled user experience in mind.</li>
+              <li>Assist and teach inexperienced trainees to get familiar with common frontend development technologies such as TypeScript, React JS, NodeJS and TailwindCSS</li>
+            </ul>
+          </Experience>
+
+          <Experience company='Enjoyneer' jobTitle='Programming Tutor' start='Nov 2021' end='March 2022' location='Hong Kong'>
+            <ul className='list-disc pl-4'>
+              <li>Teach and educate high school students on basic C programming.</li>
+              <li>Assist in constructing the syllabus, authored various documents as teaching materials for students to learn</li>
+              <li>Contributed to the design of assignments, marking, and providing feedback to students.</li>
+            </ul>
+          </Experience>
 
           <InlineSection title='Front End' titleClassName='bg-green-700/80 px-1 rounded-md text-green-200'> 
             ReactJS, NextJS, CSS, TailwindCSS 
